feb9be057eacaff72cd81f4f9649e33b894e7899
David Young <tgs.wifi@gmail.com>
2017-06-21 17:57:56 +1000

    Offscreen Canvas demo

--- dist/bundle.js
+++ dist/bundle.js
@@ -95,12 +95,6 @@ module.exports = initWebGL
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-var el = document.getElementById('messageLog')
-
-if (el !== null) {
-  el.innerText = 'Hello World'
-}
-
 const initWebGL = __webpack_require__(0)
 
 function getShaderSource (id) {
@@ -142,61 +136,125 @@ function createProgram (gl, vertexShaderSource, fragmentShaderSource) {
   return program
 }
 
-function start () {
-  var canvas = document.getElementById('glCanvas')
+function render(gl, r, g, b , a) {
+    // https://github.com/WebGLSamples/WebGL2Samples/blob/master/samples/draw_instanced.html#L98
 
-  // Initialize the GL context
-  var gl = initWebGL(canvas)
-  // Only continue if WebGL is available and working
-  if (!gl) {
-    return
-  }
-
-  // https://github.com/WebGLSamples/WebGL2Samples/blob/master/samples/draw_instanced.html#L98
-
-  // -- Init Program
-  var program = createProgram(gl, getShaderSource('vs'), getShaderSource('fs'))
-  gl.useProgram(program)
-  // -- Init Vertex Array
-  var vertexArray = gl.createVertexArray()
-  gl.bindVertexArray(vertexArray)
-  // -- Init Buffers
-  var vertexPosLocation = 0  // set with GLSL layout qualifier
-  var vertices = new Float32Array([
-    -0.3, -0.5,
-    0.3, -0.5,
-    0.0, 0.5
-  ])
-  var vertexPosBuffer = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-  gl.enableVertexAttribArray(vertexPosLocation)
-  gl.vertexAttribPointer(vertexPosLocation, 2, gl.FLOAT, false, 0, 0)
-  var vertexColorLocation = 1  // set with GLSL layout qualifier
-  var colors = new Float32Array([
-    1.0, 0.5, 0.0,
-    0.0, 0.5, 1.0
-  ])
-  var vertexColorBuffer = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW)
-  gl.enableVertexAttribArray(vertexColorLocation)
-  gl.vertexAttribPointer(vertexColorLocation, 3, gl.FLOAT, false, 0, 0)
-  gl.vertexAttribDivisor(vertexColorLocation, 1) // attribute used once per instance
-  gl.bindVertexArray(null)
-  // -- Render
-  gl.clearColor(0.0, 0.0, 0.0, 1.0)
-  gl.clear(gl.COLOR_BUFFER_BIT)
-  gl.bindVertexArray(vertexArray)
-  gl.drawArraysInstanced(gl.TRIANGLES, 0, 3, 2)
-  // -- Delete WebGL resources
-  gl.deleteBuffer(vertexPosBuffer)
-  gl.deleteBuffer(vertexColorBuffer)
-  gl.deleteProgram(program)
-  gl.deleteVertexArray(vertexArray)
+    // -- Init Program
+    let program = createProgram(gl, getShaderSource('vs'), getShaderSource('fs'))
+    gl.useProgram(program)
+    // -- Init Vertex Array
+    let vertexArray = gl.createVertexArray()
+    gl.bindVertexArray(vertexArray)
+    // -- Init Buffers
+    let vertexPosLocation = 0  // set with GLSL layout qualifier
+    let vertices = new Float32Array([
+      -0.3, -0.5,
+      0.3, -0.5,
+      0.0, 0.5
+    ])
+    let vertexPosBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+    gl.enableVertexAttribArray(vertexPosLocation)
+    gl.vertexAttribPointer(vertexPosLocation, 2, gl.FLOAT, false, 0, 0)
+    let vertexColorLocation = 1  // set with GLSL layout qualifier
+    let colors = new Float32Array([
+      1.0, 0.5, 0.0,
+      0.0, 0.5, 1.0
+    ])
+    let vertexColorBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW)
+    gl.enableVertexAttribArray(vertexColorLocation)
+    gl.vertexAttribPointer(vertexColorLocation, 3, gl.FLOAT, false, 0, 0)
+    gl.vertexAttribDivisor(vertexColorLocation, 1) // attribute used once per instance
+    gl.bindVertexArray(null)  
+    
+    // -- Render
+    gl.clearColor(r, g, b, a)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.bindVertexArray(vertexArray)
+    gl.drawArraysInstanced(gl.TRIANGLES, 0, 3, 2)
+    // -- Delete WebGL resources
+    gl.deleteBuffer(vertexPosBuffer)
+    gl.deleteBuffer(vertexColorBuffer)
+    gl.deleteProgram(program)
+    gl.deleteVertexArray(vertexArray)
 }
 
-start()
+function start (enabled, onGL, offGL) {   
+  let normal = document.getElementById('normalRendering')
+  let offRender = document.getElementById('offScreenRendering')
+
+  if (enabled) {
+    if (normal)
+      normal.style = 'display:none';
+    if (offRender)
+      offRender.style = ''
+
+    render(offGL, 1, 0, 0, 1);  
+    onGL.clearColor(0, 1, 0, 1); 
+    onGL.clear(onGL.COLOR_BUFFER_BIT)  
+  }
+  else {
+    if (offRender)
+      offRender.style = 'display:none';
+    if (normal)
+      normal.style = ''
+
+    render(onGL, 0, 1, 1, 1);    
+    offGL.clearColor(0, 1, 0, 1);
+    offGL.clear(offGL.COLOR_BUFFER_BIT)  
+  }
+
+  let bitmap = offscreen.transferToImageBitmap()
+  dest.transferImageBitmap(bitmap);  
+}
+
+let onCanvas = document.getElementById('onCanvas')
+onCanvas.addEventListener('webglcontextlost', function(e) {
+  console.log(e); 
+}, false);
+
+let offCanvas = document.getElementById('offCanvas')
+onCanvas.addEventListener('webglcontextlost', function(e) {
+  console.log(e); 
+}, false);
+
+let offscreenExtAvailable = ('transferControlToOffscreen' in onCanvas);
+let el = document.getElementById('messageLog')
+
+if (el !== null) {    
+  el.innerText =
+    offscreenExtAvailable
+    ? 'offscreen canvas found'
+    : 'offscreen canvas not found'
+}
+let cb = document.getElementById('checkBox')
+
+// Initialize the GL context
+let onGL = onCanvas.getContext('webgl2');
+// Only continue if WebGL is available and working
+if (!onGL) {
+  throw Error('on WebGL2 missing');
+}
+
+let dest = offCanvas.getContext("bitmaprenderer")
+let offscreen = new OffscreenCanvas(offCanvas.clientWidth, offCanvas.clientHeight);
+let offGL = offscreen.getContext('webgl2')
+// Only continue if WebGL is available and working
+if (!offGL) {
+  throw Error('off WebGL2 missing');
+} 
+
+start(cb.checked, onGL, offGL)
+cb.onchange = function() {
+  console.log("RENDER : " + this.checked)  
+  start(this.checked, onGL, offGL)
+
+
+}
+
 
 
 /***/ })
